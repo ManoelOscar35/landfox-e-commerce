6a3030f655996de2527819a8d9aea1ccc9cbc284
ManoelOscar35 <manoeloscar557@gmail.com>
2024-11-04 16:26:49 -0300

    implementando método put em itemRoutes.js

--- routers/itemRoutes.js
+++ routers/itemRoutes.js
@@ -1,10 +1,11 @@
 const Item = require("../models/items");
 const express = require("express");
 const router = express.Router(); 
+const mongoose = require("mongoose");
 const uploads = require('../management/multer');
 
 router.get('/', async (req, res) => {
-    const items = await Item.find();
+    const items = await Item.find().populate('category');
 
     if(!items) {
         res.status(400).send("No items found!");
@@ -45,9 +46,59 @@ router.post('/', uploads.single('image'), async (req, res) => {
     .catch((err) => {
         res.status(500).json({
             error: err,
-            message: "Impossible to create product!"
+            message: "Creation failed!"
         })
     })
+});
+
+router.put('/:id', uploads.single('image'), async (req, res) => {
+    if(!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send('Invalid Id');
+    }
+
+    const category = await Category.findById(req.body.category);
+
+    if(!category) {
+        return res.status(400).send('Invalid Category Id');
+    }
+
+    const item = await Item.findById(req.params.id);
+
+    if(!item) {
+        return res.status(400).send('Invalid item Id');
+    }
+
+    const file = req.file;
+    let image;
+
+    if(file) {
+        const fileName = file.filename;
+        const path = `${req.protocol}://${req.get('host')}/public/images/`;
+        image = `${path}${fileName}`;
+    } else {
+        image = item.image;
+    }
+
+    const modifieldItem = await Item.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            price: req.body.price,
+            image: image,
+            category: req.body.category
+        },
+        {
+            new: true
+        }
+    );
+
+    if(!modifieldItem) {
+        return res.status(500).send('The item cannot be updated!');
+    }
+
+    res.send(modifieldItem);
+
 });
 
 module.exports = router;
